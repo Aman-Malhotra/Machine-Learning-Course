8d4f5fb758f8e06da3d15c91116047f3c9e12e5e
aman-malhotra-1999 <official.aman18@gmail.com>
2019-05-02 18:20:26 +0530

    Random Forest Tree Regression

--- Part 2 Regression/Section 9 Random Forest Tree Regression/random_forest_tree.py
+++ Part 2 Regression/Section 9 Random Forest Tree Regression/random_forest_tree.py
@@ -1,4 +1,4 @@
-# Decision Tree Regression
+#Random Forst Regression Tree
 
 # Importing the libraries
 import numpy as np
@@ -10,20 +10,20 @@ dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
-# Fitting the Decision Tree Regression Model to the dataset
-from sklearn.tree import DecisionTreeRegressor
-regressor = DecisionTreeRegressor(random_state = 0)
+# Fitting the Regression Model to the dataset
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators=300, random_state=0)
 regressor.fit(X, y)
 
 # Predicting a new result
 y_pred = regressor.predict([[6.5]])
 
-# Visualising the Regression results (for higher resolution and smoother curve)
+# Visualising the Random Forest Regression results (for higher resolution and smoother curve)
 X_grid = np.arange(min(X), max(X), 0.01)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
 plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
-plt.title('Truth or Bluff (Decision Tree Regression Model)')
+plt.title('Truth or Bluff (Random Forest Regression Model)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
